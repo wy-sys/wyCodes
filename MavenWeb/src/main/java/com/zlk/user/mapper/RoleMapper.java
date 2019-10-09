d393a8bf665529aa0cd2283f57436e0fe5f89a87
wy-sys <w13939387905@163.com>
2019-10-09 14:20:23 +0800

    delete file

--- MavenWeb/src/main/java/com/zlk/user/mapper/RoleMapper.java
+++
@@ -1,9 +0,0 @@
-package com.zlk.user.mapper;
-
-import com.zlk.user.entity.UserFunction;
-
-import java.util.List;
-
-public interface RoleMapper {
-    List<UserFunction> findFunByName(String uname);
-}
--- MavenWeb/src/main/java/com/zlk/user/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.zlk.user.service;
-
-import com.zlk.user.entity.UserFunction;
-
-import java.util.List;
-
-public interface RoleService {
-    List<UserFunction> findFunByName(String uname);
-}
